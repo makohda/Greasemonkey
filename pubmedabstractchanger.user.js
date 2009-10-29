0e20bc7ba018dc9ae87636863ef964e1affc12be
Kohda Masakazu <ma.kohda@gmail.com>
2009-10-29 12:37:57 +0900

    add: pubmedabstractchanger.user.js

---
+++ pubmedabstractchanger.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name           PubMedAbstractChanger
+// @namespace      id:ma_ko
+// @include        http://www.ncbi.nlm.nih.gov/pubmed/*
+// ==/UserScript==
+GM_addStyle(".abstract_text { font-size: medium; }");
